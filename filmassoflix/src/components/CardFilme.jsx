df8ab8a4978df4bd3fb87b8d8e1dd97625d189b0
PedroHCarlini <phcarlini@gmail.com>
2023-10-03 20:25:07 -0300

    a

--- filmassoflix/src/components/CardFilme.jsx
+++ filmassoflix/src/components/CardFilme.jsx
@@ -12,22 +12,24 @@ export default function CardFilme({filme}){
     const movieId =`https://www.themoviedb.org/movie/${filme.id}`
 
     return (
-        <div id="card" key={filme.id} className='flex flex-col w-40 justify-center items-center m-2'>
-            { favorito ? 
-                <HeartIcon onClick={() => desfavoritar(filme)} className="h-6 w-6 text-rose-600 cursor-pointer " />
-            :
-                <HeartIcon onClick={() => favoritar(filme)}  className="h-6 w-6 text-slate-100 cursor-pointer" />
-            }
-            <img className='rounded' src={url_imagem} alt="Movie" />
-            <span className='font-bold text-center line-clamp-1'>
-                {filme.title}
-            </span>
-            <div>
-                <span>{filme.vote_average.toFixed(1)}</span>
+        <div key={filme.id} >
+            <div id="card" className='flex flex-col w-40 justify-center items-center m-2'>
+                { favorito ? 
+                    <HeartIcon onClick={() => desfavoritar(filme)} className="h-6 w-6 text-rose-600 cursor-pointer " />
+                :
+                    <HeartIcon onClick={() => favoritar(filme)}  className="h-6 w-6 text-slate-100 cursor-pointer" />
+                }
+                <img className='rounded' src={url_imagem} alt="Movie" />
+                <span className='font-bold text-center line-clamp-1'>
+                    {filme.title}
+                </span>
+                <div>
+                    <span>{filme.vote_average.toFixed(1)}</span>
+                </div>
+                    <a href={movieId} target='_blank' className='bg-pink-600 py-2 w-full rounded text-center'>
+                        detalhes
+                    </a>
             </div>
-                <a href={movieId} target='_blank' className='bg-pink-600 py-2 w-full rounded text-center'>
-                    detalhes
-                </a>
         </div>
     )
 }
